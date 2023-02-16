d9b5f844a3ac3ecce08b857eca779537871f2f1d
dobble11 <1103650146@qq.com>
2023-02-16 23:11:32 +0800

    test: url

--- vite-react/e2e/app.test.tsx
+++ vite-react/e2e/app.test.tsx
@@ -2,11 +2,11 @@ jest.setTimeout(5 * 60 * 1000);
 
 describe('App', () => {
   beforeAll(async () => {
-    await page.goto('http://127.0.0.1:5173');
+    await page.goto('http://localhost:5173');
   });
 
   it('单击按钮+1', async () => {
-    const element = await page.waitForSelector('.card > button');
+    const element = (await page.waitForSelector('.card > button'))!;
 
     await element.click();
     expect(element.evaluate((el) => el.textContent)).resolves.toBe('count is 1');
